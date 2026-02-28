f66e9a0d41a8770b687a029d08e3e93a6165141b
Arif Raza <arif3raza@gmail.com>
2026-02-28 20:26:23 +0530

    Create Home.js

---
+++ Frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import CarList from "../components/CarList";
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>Available Cars</h1>
+      <CarList />
+    </div>
+  );
+}
+
+export default Home;
